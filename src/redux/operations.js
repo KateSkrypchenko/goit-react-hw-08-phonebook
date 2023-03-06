f568484fdbdd4c18420be1399a6e6f953bf33d24
user <katalonij440@gmail.com>
2023-03-06 19:26:30 +0200

    start

---
+++ src/redux/operations.js
@@ -0,0 +1,34 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://63e6235d83c0e85a868d680a.mockapi.io';
+
+export const fetchContacts = createAsyncThunk('contacts/fetchAll', async (_, thunkAPI) => {
+  try {
+    const response = await axios.get('/contacts');
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+export const addContacts = createAsyncThunk('contacts/addContacts', async (contact, thunkAPI) => {
+  try {
+    const response = await axios.post('/contacts', contact);
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+export const deleteContacts = createAsyncThunk(
+  'contacts/deleteContacts',
+  async (contactId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${contactId}`);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
